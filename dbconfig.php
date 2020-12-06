869c2e88428f24ffd14a27b733f5cc943a3d00d6
kblake16 <57978429+kblake16@users.noreply.github.com>
2020-12-05 19:54:32 -0500

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'locahost';
-$dbname = 'schemaDB';
-$username ='admin';
-$password = 'UserAdmin1';
-
-/*INSERT INTO UserTable(firstname, lastname, password, email, date_joined)
-  VALUES ('user', 'admin', password_hash('password',PASSWORD_DEFAULT), 'admin@project2.com','2020-12-1'); hash password*/
